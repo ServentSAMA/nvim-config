def09541fb2722dc5a04c47897655b078bbcdf18
ShenChuanBiao <1055889855@qq.com>
2024-06-04 21:23:55 +0800

    加一些配置

--- lua/plugins/plugin-neo-tree.lua
+++ lua/plugins/plugin-neo-tree.lua
@@ -1,13 +1,37 @@
 return {
     {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-          "nvim-lua/plenary.nvim",
-          "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-          "MunifTanjim/nui.nvim",
-          -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+      "nvim-neo-tree/neo-tree.nvim",
+      branch = "v3.x",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+        -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+      },
+      opts = {
+        source_selector = { -- 控制显示文件树顶部样式
+          winbar = true,
+          statusline = true
         },
-       close_if_last_window = false,
+        filesystem = {
+          filtered_items = {
+            hide_dotfiles = false, -- 控制是否显示以.开头的文件夹和文件
+            hide_hidden = false, -- 控制是否显示隐藏文件，win独有
+          },
+          window = {
+            mappings = { -- 映射快捷键到操作
+              ["<F5>"] = "refresh",
+              ["o"] = "open",
+            }
+          },
+        },
+      },
+      keys = {
+          { "<c-n>", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
+      },
+      close_if_last_window = false,
+      -- config = function ()
+      --       require("neo-tree")
+      -- end
     }
 }
